c8d8a28f040aa432ef42b6375fae9b0138822f56
Georgia <ghobs987@gmail.com>
2022-02-22 23:59:02 +0000

    grid for display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,30 @@
 import React from "react";
 import "./App.css";
 import { Cutaway } from "./components/Cutaway";
-import  background  from './assets/images/background.png';
+import background from "./assets/images/fullsizebackground.png";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
-    <div className="background"
+    <div
+      className="background"
       style={{ backgroundImage: `url(${background})` }}
     >
+      <div>tl</div>
 
-    <div
-      // className="App"
-      >
-      <Cutaway />
-    </div>
+      <div>tm</div>
+      <div>
+        <Nav />
       </div>
+      <div>ml</div>
+      <div className="cutawaycontainer">
+        <Cutaway />
+      </div>
+      <div>mr</div>
+      <div>bl</div>
+      <div>bm</div>
+      <div>br</div>
+    </div>
   );
 }
 
