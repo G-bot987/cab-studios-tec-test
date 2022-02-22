b8dde928a35729362803ba2bae48e0acd618b3ad
Georgia <ghobs987@gmail.com>
2022-02-22 01:41:21 +0000

    background img import and cutaway styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
+import React from "react";
 import "./App.css";
 import { Cutaway } from "./components/Cutaway";
+import  background  from './assets/images/background.png';
 
 function App() {
   return (
-    <div className="App">
+    <div className="background"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+
+    <div
+      // className="App"
+      >
       <Cutaway />
     </div>
+      </div>
   );
 }
 
