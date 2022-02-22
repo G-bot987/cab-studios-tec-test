9de060d7c167e5cde0de96dc5a80d36872dbd5f3
Georgia <ghobs987@gmail.com>
2022-02-22 04:06:58 +0000

    cutaway styling

--- my-app/src/components/Cutaway.js
+++ my-app/src/components/Cutaway.js
@@ -1,5 +1,15 @@
 import React from "react";
 import "./Cutaway.css";
 export const Cutaway = () => {
-  return <div id="cutaway">test</div>;
+  return (
+    <div>
+      <div id="wrapper">
+        <div id="topleft"></div>
+        <div id="topright"></div>
+        <div id="bottomleft"></div>
+        <div id="midright"></div>
+        <div id="bottomright"></div>
+      </div>
+    </div>
+  );
 };
