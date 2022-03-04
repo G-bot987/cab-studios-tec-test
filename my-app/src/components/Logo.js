a9600242ef50f4bd40eaac14fcf55474a0f73e26
Georgia <ghobs987@gmail.com>
2022-03-04 17:40:55 +0000

    letter imports and styling for letters, needs refracting

--- my-app/src/components/Logo.js
+++ my-app/src/components/Logo.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 import "./Logo.css";
+import E from "../assets/images/E.png";
+import C from "../assets/images/C.png";
+import A from "../assets/images/A.png";
+import L from "../assets/images/L.png";
+import P from "../assets/images/P.png";
+import Y from "../assets/images/Y.png";
+import D from "../assets/images/D.png";
+import N from "../assets/images/N.png";
+import I from "../assets/images/I.png";
+import R from "../assets/images/R.png";
+import B from "../assets/images/B.png";
+import O from "../assets/images/O.png";
+import W from "../assets/images/W.png";
+import T from "../assets/images/T.png";
+
 export const Logo = () => {
+
+
+
+
   return (
-    <div className='logoContainer'>
-       <p className='logo'> TWO <br></br> BRINDLEY PLACE </p>
+    <div className="logoContainer">
+      
+     
+      <div id="E" style={{ backgroundImage: `url(${E})` }}></div>
+      <div id="C" style={{ backgroundImage: `url(${C})` }}></div>
+      <div id="A" style={{ backgroundImage: `url(${A})` }}></div>
+      <div id="L" style={{ backgroundImage: `url(${L})` }}></div>
+      <div id="P" style={{ backgroundImage: `url(${P})` }}></div>
+      <div id="Y" style={{ backgroundImage: `url(${Y})` }}></div>
+      <div id="E1" style={{ backgroundImage: `url(${E})` }}></div>
+      <div id="L1" style={{ backgroundImage: `url(${L})` }}></div>
+      <div id="D" style={{ backgroundImage: `url(${D})` }}></div>
+      <div id="N" style={{ backgroundImage: `url(${N})` }}></div>
+      <div id="I" style={{ backgroundImage: `url(${I})` }}></div>
+      <div id="R" style={{ backgroundImage: `url(${R})` }}></div>
+      <div id="B" style={{ backgroundImage: `url(${B})` }}></div>
+      <div id="O" style={{ backgroundImage: `url(${O})` }}></div>
+      <div id="W" style={{ backgroundImage: `url(${W})` }}></div>
+      <div id="T" style={{ backgroundImage: `url(${T})` }}></div>
     </div>
-  )
-}
+  );
+};
