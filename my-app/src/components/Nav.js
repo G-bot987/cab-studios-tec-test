bbc4d06dda53e1025b0ac46850f3a2d9c7f491a7
Georgia <ghobs987@gmail.com>
2022-03-09 04:08:39 +0000

    navbar a tag use

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
 import "./Nav.css";
 export const Nav = () => {
   return (
-    <div className='Nav'>
-      <p className='icon'>WELCOME</p>
-      <p className='icon'> DOWNLOADS</p>
-      <p className='icon'>CONTACT</p>
-      
+    <div className="Nav">
+      <ul
+        style={{
+          margin: 0,
+          listStyle: "none",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          marginRight: 100,
+        }}
+      >
+        <li className="navbar-item">
+          <a href="#">WELCOME</a>
+        </li>
+        <li
+          className="navbar-item"
+          style={{
+            margin: "0 2rem",
+          }}
+        >
+          <a href="#">DOWNLOADS</a>
+        </li>
+        <li className="navbar-item">
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
